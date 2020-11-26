d788650e766b1fb3f1842bf55a09b231a83ad631
user <user@192.168.1.10>
2020-11-26 19:37:47 +0530

    Added maven plugins

---
+++ CoachingClass/run.bat
@@ -0,0 +1,2 @@
+cd C:\Users\user\git\repository\CoachingClass
+mvn clean install
--- CoachingClass/src/test/java/runner/TestRunnerNG.java
+++ CoachingClass/src/test/java/runner/TestRunnerNG.java
@@ -1,19 +1,15 @@
 package runner;
 
-import org.junit.runner.RunWith;
-import cucumber.api.junit.Cucumber;
-
 import cucumber.api.CucumberOptions;
+import cucumber.api.testng.AbstractTestNGCucumberTests;
 
-@RunWith(Cucumber.class)
+//@RunWith(Cucumber.class)
 @CucumberOptions(
 		features="features/register.feature"
 		,glue="stepDefinitions"
 		,plugin = {"html:target/Destination"}
 			)
 
-public class TestRunner {
+public class TestRunnerNG extends AbstractTestNGCucumberTests {
 
 }
-
-
